67383a955c03b0ba0a79f9eadf9cdf60828cbc61
RoyGuo <253583536@qq.com>
2016-08-04 13:06:00 +0800

    1.backup

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ import time                         #获取当前时间
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
-conn = pymongo.MongoClient(host="127.0.0.1", port=27017)
+conn = pymongo.MongoClient(host="172.16.10.50", port=27017)
 db = conn['RoyDB']
 print(db.collection_names())
 collect = db['TJ']
